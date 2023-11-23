22a2ad2d2dc532c19287dfbf8f49a2d9f0fb7f36
lvyanquan <decq12ybhl@gmail.com>
2023-11-23 20:30:05 +0800

    [cdc-runtime] update DataSinkWriterOperator to proxy the execution of SinkWriterOperator. flinkWriterOperator and copySinkWriter were created by reflection because they are not public Visibility.

--- flink-cdc-runtime/src/main/java/com/ververica/cdc/runtime/operators/sink/DataSinkWriterOperator.java
+++ flink-cdc-runtime/src/main/java/com/ververica/cdc/runtime/operators/sink/DataSinkWriterOperator.java
@@ -18,9 +18,14 @@ package com.ververica.cdc.runtime.operators.sink;
 
 import org.apache.flink.api.common.operators.MailboxExecutor;
 import org.apache.flink.api.connector.sink2.Sink;
+import org.apache.flink.api.connector.sink2.SinkWriter;
 import org.apache.flink.runtime.jobgraph.OperatorID;
 import org.apache.flink.runtime.state.StateInitializationContext;
+import org.apache.flink.streaming.api.connector.sink2.CommittableMessage;
 import org.apache.flink.streaming.api.graph.StreamConfig;
+import org.apache.flink.streaming.api.operators.AbstractStreamOperator;
+import org.apache.flink.streaming.api.operators.BoundedOneInput;
+import org.apache.flink.streaming.api.operators.OneInputStreamOperator;
 import org.apache.flink.streaming.api.operators.Output;
 import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
 import org.apache.flink.streaming.runtime.tasks.ProcessingTimeService;
@@ -30,35 +35,73 @@ import com.ververica.cdc.common.annotation.Internal;
 import com.ververica.cdc.common.event.Event;
 import com.ververica.cdc.common.event.FlushEvent;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
 /**
  * An operator that processes records to be written into a {@link
- * org.apache.flink.api.connector.sink2.Sink}. It also has a way to process committables with the
- * same parallelism or send them downstream to a {@link CommitterOperator} with a different
- * parallelism.
+ * org.apache.flink.api.connector.sink2.Sink}.
+ *
+ * <p>The operator is a proxy of {@link
+ * org.apache.flink.streaming.runtime.operators.sink.SinkWriterOperator}.
  *
  * <p>The operator is always part of a sink pipeline and is the first operator.
  *
  * @param <CommT> the type of the committable (to send to downstream operators)
  */
 @Internal
-public class DataSinkWriterOperator<CommT> extends SinkWriterOperator<Event, CommT> {
+public class DataSinkWriterOperator<CommT> extends AbstractStreamOperator<CommittableMessage<CommT>>
+        implements OneInputStreamOperator<Event, CommittableMessage<CommT>>, BoundedOneInput {
 
     private SchemaEvolutionClient schemaEvolutionClient;
 
     private final OperatorID schemaOperatorID;
+
+    Sink<Event> sink;
+
+    ProcessingTimeService processingTimeService;
+
+    MailboxExecutor mailboxExecutor;
+
+    /** Operator that actually execute sink logic. */
+    Object flinkWriterOperator;
+
+    /**
+     * The internal {@link SinkWriter} of flinkWriterOperator, obtained it through reflection to
+     * deal with {@link FlushEvent}.
+     */
+    private SinkWriter<Event> copySinkWriter;
 
     public DataSinkWriterOperator(
             Sink<Event> sink,
             ProcessingTimeService processingTimeService,
             MailboxExecutor mailboxExecutor,
             OperatorID schemaOperatorID) {
-        super(sink, processingTimeService, mailboxExecutor);
+        this.sink = sink;
+        this.processingTimeService = processingTimeService;
+        this.mailboxExecutor = mailboxExecutor;
         this.schemaOperatorID = schemaOperatorID;
     }
 
     @Override
     public void setup(StreamTask containingTask, StreamConfig config, Output output) {
         super.setup(containingTask, config, output);
+        try {
+            Class<?> flinkWriterClass =
+                    getRuntimeContext()
+                            .getUserCodeClassLoader()
+                            .loadClass(
+                                    "org.apache.flink.streaming.runtime.operators.sink.SinkWriterOperator");
+            Constructor<?> constructor =
+                    flinkWriterClass.getDeclaredConstructor(
+                            Sink.class, ProcessingTimeService.class, MailboxExecutor.class);
+            constructor.setAccessible(true);
+            flinkWriterOperator =
+                    constructor.newInstance(sink, processingTimeService, mailboxExecutor);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        ((AbstractStreamOperator) flinkWriterOperator).setup(containingTask, config, output);
         schemaEvolutionClient =
                 new SchemaEvolutionClient(
                         containingTask.getEnvironment().getOperatorCoordinatorEventGateway(),
@@ -67,24 +110,61 @@ public class DataSinkWriterOperator<CommT> extends SinkWriterOperator<Event, Com
 
     @Override
     public void open() throws Exception {
-        super.open();
+        ((AbstractStreamOperator) flinkWriterOperator).open();
+        copySinkWriter = (SinkWriter) getFieldValue("sinkWriter");
+    }
+
+    /**
+     * Finds a field by name from its declaring class. This also searches for the field in super
+     * classes.
+     *
+     * @param fieldName the name of the field to find.
+     * @return the Object value of this field.
+     */
+    private Object getFieldValue(String fieldName) throws IllegalAccessException {
+        Class clazz = flinkWriterOperator.getClass();
+        while (clazz != null) {
+            try {
+                Field field = clazz.getDeclaredField(fieldName);
+                field.setAccessible(true);
+                return field.get(flinkWriterOperator);
+            } catch (NoSuchFieldException e) {
+                clazz = clazz.getSuperclass();
+            }
+        }
+        throw new RuntimeException("failed to get sinkWriter");
     }
 
     @Override
     public void initializeState(StateInitializationContext context) throws Exception {
-        super.initializeState(context);
         schemaEvolutionClient.registerSubtask(getRuntimeContext().getIndexOfThisSubtask());
+        ((AbstractStreamOperator) flinkWriterOperator).initializeState(context);
     }
 
     @Override
     public void processElement(StreamRecord<Event> element) throws Exception {
         Event event = element.getValue();
         if (event instanceof FlushEvent) {
-            sinkWriter.flush(false);
+            copySinkWriter.flush(false);
             schemaEvolutionClient.notifyFlushSuccess(
                     getRuntimeContext().getIndexOfThisSubtask(), ((FlushEvent) event).getTableId());
         } else {
-            super.processElement(element);
+            ((OneInputStreamOperator) flinkWriterOperator).processElement(element);
         }
+    }
+
+    @Override
+    public void prepareSnapshotPreBarrier(long checkpointId) throws Exception {
+        ((AbstractStreamOperator) flinkWriterOperator).prepareSnapshotPreBarrier(checkpointId);
+    }
+
+    @Override
+    public void close() throws Exception {
+        ((OneInputStreamOperator) flinkWriterOperator).close();
+    }
+
+    @Override
+    public void endInput() throws Exception {
+        ((BoundedOneInput) flinkWriterOperator).endInput();
     }
 }
